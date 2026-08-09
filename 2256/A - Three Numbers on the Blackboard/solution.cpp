4bdfcbaa8ca655d265771f733b3114019df62c30
Nitin Saroj <nitinsaroj1732005@gmail.com>
2026-08-09 20:22:22 +0530

    Add Three Numbers on the Blackboard [A] from Codeforces

---
+++ 2256/A - Three Numbers on the Blackboard/solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<int> a(3);
+        for(int i=0;i<3;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        if(a[0]+a[1]>=a[2]){
+            cout<<a[2]-a[0]<<endl;
+        }
+        else{
+            while(a[0]+a[1]<a[2]){
+            a[2]=a[0]+a[1];
+            }
+            cout<<a[2]-a[0]<<endl;
+        }
+    }
+}
