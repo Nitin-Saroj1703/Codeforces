5068538f01c0d881568feeb1f28e9bef56437f14
Nitin Saroj <nitinsaroj1732005@gmail.com>
2026-09-06 17:40:18 +0530

    Add Permutation Swap [B] from Codeforces

---
+++ 1828/B - Permutation Swap/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int diff=arr[0]-1;
+        for(int i=1;i<n;i++){
+            diff=__gcd(abs(arr[i]-(i+1)),diff);
+        }
+        cout<<diff<<endl;
+    }
+}
